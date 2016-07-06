e781624ff21207d63657367fb2c9954c16bffe35
C-Elegans <mtnolan2640@gmail.com>
2016-07-05 21:12:45 -0400

    Update README, change indentation in Peephole.cpp

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 This is a work in progress compiler for the [D16 cpu](https://github.com/C-Elegans/d16) defined in another project.
 The compiler compiles a small subset of C, called C-, which is defined [here](http://www.cs.dartmouth.edu/~cs57/Project/C-%20Spec.pdf).
 
-Currently the compiler parses the input file and builds an AST which it subsequently prints. Code generation will come soon.
+Currently the compiler parses the input file and builds an AST which it subsequently prints.
 ## Building
 Linux and OS X:
 
-(Requires bison and flex)
+(Requires bison, flex, and a c++14 compliant compiler)
 ```
-./autogen.sh
-./configure
+mkdir build
+cd build
+cmake ..
 make
-make install
 ```
--- src/Peephole.cpp
+++ src/Peephole.cpp
@@ -8,20 +8,20 @@
 #include "Instruction_RR.hpp"
 
 Peephole::Peephole() {
-/*
- * converts:
- *      push r0
- *      pop r0
- * into:
- *      (nothing)
- *
- */
+    /*
+     * converts:
+     *      push r0
+     *      pop r0
+     * into:
+     *      (nothing)
+     *
+     */
     passes.push_back([](instruction_vec instructions) {
-        for (int i = 0; i < instructions.size() - 1; i++) {
-            if (instructions[i]->type == PUSH && instructions[i + 1]->type == POP) {
-                if (instanceOf<Instruction_RR>(&(*instructions[i]))) {
-                    if (dynamic_cast<Instruction_RR &>(*instructions[i]).rD ==
-                        dynamic_cast<Instruction_RR &>(*instructions[i + 1]).rD) {
+        for(int i = 0; i < instructions.size() - 1; i++) {
+            if(instructions[i]->type == PUSH && instructions[i + 1]->type == POP) {
+                if(instanceOf<Instruction_RR>(&(*instructions[i]))) {
+                    if(dynamic_cast<Instruction_RR &>(*instructions[i]).rD ==
+                       dynamic_cast<Instruction_RR &>(*instructions[i + 1]).rD) {
                         instructions.erase(instructions.begin() + i, instructions.begin() + i + 2);
                     }
                 }
@@ -37,15 +37,16 @@ Peephole::Peephole() {
      *      mov r0, #1
      */
     passes.push_back([](instruction_vec instructions) {
-        for (int i = 0; i < instructions.size() - 1; i++) {
-            if (instructions[i]->type == PUSH && instructions[i + 1]->type == POP) {
-                if (instanceOf<Instruction_RI>(&(*instructions[i]))) {
+        for(int i = 0; i < instructions.size() - 1; i++) {
+            if(instructions[i]->type == PUSH && instructions[i + 1]->type == POP) {
+                if(instanceOf<Instruction_RI>(&(*instructions[i]))) {
 
 
-                    std::unique_ptr<Instruction_RI> inst = std::make_unique<Instruction_RI>(MOV,
-                                                                                            dynamic_cast<Instruction_RR &>(*instructions[
-                                                                                                    i + 1]).rD,
-                                                                                            dynamic_cast<Instruction_RI &>(*instructions[i]).immediate
+                    std::unique_ptr<Instruction_RI> inst =
+                        std::make_unique<Instruction_RI>(MOV,
+                        dynamic_cast<Instruction_RR &>(*instructions[
+                            i + 1]).rD,
+                        dynamic_cast<Instruction_RI &>(*instructions[i]).immediate
                     );
                     instructions.erase(instructions.begin() + i, instructions.begin() + i + 2);
                     instructions.insert(instructions.begin() + i, std::move(inst));
@@ -64,15 +65,15 @@ Peephole::Peephole() {
      *      mov r1,#1
      */
     passes.push_back([](instruction_vec instructions) {
-        for (int i = 0; i < instructions.size() - 2; i++) {
-            if (instructions[i]->type == PUSH && instructions[i + 2]->type == POP) {
-                if (instanceOf<Instruction_RR>(&(*instructions[i]))) {
+        for(int i = 0; i < instructions.size() - 2; i++) {
+            if(instructions[i]->type == PUSH && instructions[i + 2]->type == POP) {
+                if(instanceOf<Instruction_RR>(&(*instructions[i]))) {
                     int rd = dynamic_cast<Instruction_RR &>(*instructions[i]).rD;
-                    if (rd == dynamic_cast<Instruction_RR &>(*instructions[i + 2]).rD) {
-                        if ((instanceOf<Instruction_RR>(&(*instructions[i + 1])) &&
-                             rd != dynamic_cast<Instruction_RR &>(*instructions[i + 1]).rD) ||
-                            (instanceOf<Instruction_RI>(&(*instructions[i + 1])) &&
-                             rd != dynamic_cast<Instruction_RI &>(*instructions[i + 1]).rD)) {
+                    if(rd == dynamic_cast<Instruction_RR &>(*instructions[i + 2]).rD) {
+                        if((instanceOf<Instruction_RR>(&(*instructions[i + 1])) &&
+                            rd != dynamic_cast<Instruction_RR &>(*instructions[i + 1]).rD) ||
+                           (instanceOf<Instruction_RI>(&(*instructions[i + 1])) &&
+                            rd != dynamic_cast<Instruction_RI &>(*instructions[i + 1]).rD)) {
                             instructions.erase(instructions.begin() + i + 2);
                             instructions.erase(instructions.begin() + i);
 
@@ -93,18 +94,20 @@ Peephole::Peephole() {
      * into:
      *      (op) r0,#1
      */
-    passes.push_back([](instruction_vec instructions){
-        for(int i=0;i<instructions.size()-1;i++){
-            if(instructions[i]->type == MOV){
-                if(instanceOf<Instruction_RI>(&(*instructions[i])) && instanceOf<Instruction_RR>(&(*instructions[i+1]))){
-                    int rd_1 = static_cast<Instruction_RI&>(*instructions[i]).rD;
-                    int imm = static_cast<Instruction_RI&>(*instructions[i]).immediate;
-                    int rs_2 = static_cast<Instruction_RR&>(*instructions[i+1]).rS;
-                    int rd_2 = static_cast<Instruction_RR&>(*instructions[i+1]).rD;
-                    OpType type = instructions[i+1]->type;
-                    if(rd_1 == rs_2){
-                        instructions.erase(instructions.begin()+i, instructions.begin()+i+2);
-                        instructions.insert(instructions.begin()+i,std::make_unique<Instruction_RI>(type,rd_2, imm));
+    passes.push_back([](instruction_vec instructions) {
+        for(int i = 0; i < instructions.size() - 1; i++) {
+            if(instructions[i]->type == MOV) {
+                if(instanceOf<Instruction_RI>(&(*instructions[i])) &&
+                   instanceOf<Instruction_RR>(&(*instructions[i + 1]))) {
+                    int rd_1 = static_cast<Instruction_RI &>(*instructions[i]).rD;
+                    int imm = static_cast<Instruction_RI &>(*instructions[i]).immediate;
+                    int rs_2 = static_cast<Instruction_RR &>(*instructions[i + 1]).rS;
+                    int rd_2 = static_cast<Instruction_RR &>(*instructions[i + 1]).rD;
+                    OpType type = instructions[i + 1]->type;
+                    if(rd_1 == rs_2) {
+                        instructions.erase(instructions.begin() + i, instructions.begin() + i + 2);
+                        instructions.insert(instructions.begin() + i,
+                                            std::make_unique<Instruction_RI>(type, rd_2, imm));
                     }
                 }
             }
@@ -115,7 +118,7 @@ Peephole::Peephole() {
 }
 
 instruction_vec Peephole::run_passes(instruction_vec instructions) {
-    for (int i = 0; i < passes.size(); i++) {
+    for(int i = 0; i < passes.size(); i++) {
         instructions = std::move(passes[i](std::move(instructions)));
     }
     return instructions;
